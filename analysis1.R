391af32a5bbf8b3cccdd747e7bdd35760577be11
Lukas G. Aarhus <lukas.grahl@econ.au.dk>
2025-05-05 09:27:34 +0200

    added first R file

---
+++ analysis1.R
@@ -0,0 +1,11 @@
+gc()
+
+library(tidyverse)
+library(haven)
+library(fixest)
+
+# load data
+data_dir <- "C:/Users/au782406/Documents/GIT/adv_metrics_25/data"
+df <- read_dta(file.path(data_dir, "HRS_long.dta"))
+
+
