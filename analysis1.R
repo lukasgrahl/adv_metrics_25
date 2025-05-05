07e51c14b4a60e96623d01feb92c785455508aed
Lukas G. Aarhus <lukas.grahl@econ.au.dk>
2025-05-05 09:44:09 +0200

    added typst file

--- analysis1.R
+++ analysis1.R
@@ -4,6 +4,8 @@ library(tidyverse)
 library(haven)
 library(fixest)
 
+setwd("")
+
 # load data
 data_dir <- "C:/Users/au782406/Documents/GIT/adv_metrics_25/data"
 df <- read_dta(file.path(data_dir, "HRS_long.dta"))
