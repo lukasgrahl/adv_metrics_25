42eccfbcd9826a15e075961e53fe76fea3d539a7
Lukas G. Aarhus <lukas.grahl@econ.au.dk>
2025-05-15 14:37:37 +0200

    higher numbers -> more up to date

--- analysis1.R
+++ analysis1.R
@@ -3,11 +3,45 @@ gc()
 library(tidyverse)
 library(haven)
 library(fixest)
+library(did)
 
-setwd("")
+
+setwd("C:/Users/au782406/Documents/GIT/adv_metrics_25")
 
 # load data
-data_dir <- "C:/Users/au782406/Documents/GIT/adv_metrics_25/data"
-df <- read_dta(file.path(data_dir, "HRS_long.dta"))
+data_dir <- "data"
+data <- read_dta(file.path(data_dir, "HRS_long.dta"))
+
+df <- data
+## filter data
+# non-pregnancy hospitalisation
 
 
+# adults hospitalised 50-59
+f <- df$age_hosp >= 50 & df$age_hosp <= 59
+df <- filter(df, f)
+
+
+# subsample of indiviudals in waves (7-11) (2004 - 2012)+
+f <- (df$wave >= 7) & (df$wave <= 11)
+df <- filter(df, f)
+
+# summary stats
+summary(
+  df[c("sagey_e", "ragender", "raracem", "riwbegy")]
+)
+
+df['hospital_duration'] <- df$riwend - df$riwbegy
+
+
+df[c("riwbegy", "riwend")]
+
+ggplot(
+  df, aes(hacohort, riwend)
+) + geom_point()
+
+
+# Sant'Anna Callaway
+agg_gt(
+  yname=
+)
